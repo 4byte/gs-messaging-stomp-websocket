254eab6a2bafed584181dc1cc4ec2c24d6d595ec
4byte <medhigh.medhigh@gmail.com>
2020-03-29 05:56:03 +0300

    trying to set own binary ws handler

--- complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
+++ complete/src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -2,18 +2,20 @@ package com.example.messagingstompwebsocket;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.util.HtmlUtils;
 
-@Controller
+@Controller("/")
 public class GreetingController {
 
-
-	@MessageMapping("/hello")
-	@SendTo("/topic/greetings")
-	public Greeting greeting(HelloMessage message) throws Exception {
-		Thread.sleep(1000); // simulated delay
-		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	@MessageMapping("/greeting")
+	public BinaryMessage greeting2(WebSocketMessage<BinaryMessage> data) {
+		System.out.println("RECEIVED!!");
+		return data.getPayload();
 	}
 
 }
